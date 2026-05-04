2fd46a6d9347b33e9b36d6156b51d4a88efd0397
cfurl <chad.furl@gmail.com>
2026-05-04 09:22:16 -0500

    done adding texas_mrb

--- add_area_aws/upload_step8_aws.R
+++ add_area_aws/upload_step8_aws.R
@@ -1,3 +1,6 @@
+# This script uploads your 1) boundary mask, 2) area vol calc mask, and 3) cells.gpkg
+
+
 suppressPackageStartupMessages({
   library(paws)
 })
@@ -24,8 +27,9 @@ s3 <- paws::s3(config = list(region = "us-east-2"))
 bucket <- "stg4-24hr-aws-pipeline"
 
 # ---------- Upload 1 ----------
-local_1 <- "C:/stg4-hrap-gis/layers/cape_fear3/prepped/aws/nh_cf-boundary-mask.parquet"
-key_1   <- "CONUS_subset/config/aoi/nh_cf/assets/nh_cf-boundary-mask.parquet"
+
+local_1 <- "C:/stg4-hrap-gis/layers/texas_mrb/area_setup/prepped/aws/texas_mrb-boundary-mask.parquet"
+key_1   <- "CONUS_subset/config/aoi/texas_mrb/assets/texas_mrb-boundary-mask.parquet"
 
 raw_1 <- readBin(local_1, what = "raw", n = file.info(local_1)$size)
 s3$put_object(
@@ -37,8 +41,8 @@ s3$put_object(
 message("Uploaded: s3://", bucket, "/", key_1)
 
 # ---------- Upload 2 ----------
-local_2 <- "C:/stg4-hrap-gis/layers/cape_fear3/prepped/aws/nh_cf-area-vol-calc-masks.parquet"
-key_2   <- "CONUS_subset/config/aoi/nh_cf/assets/nh_cf-area-vol-calc-masks.parquet"
+local_2 <- "C:/stg4-hrap-gis/layers/texas_mrb/area_setup/prepped/aws/texas_mrb-area-vol-calc-masks.parquet"
+key_2   <- "CONUS_subset/config/aoi/texas_mrb/assets/texas_mrb-area-vol-calc-masks.parquet"
 
 raw_2 <- readBin(local_2, what = "raw", n = file.info(local_2)$size)
 s3$put_object(
@@ -48,3 +52,20 @@ s3$put_object(
   ContentType = "application/octet-stream"
 )
 message("Uploaded: s3://", bucket, "/", key_2)
+
+# ---------- Upload 3 ----------
+# (GeoPackage: cells.gpkg)
+
+local_3 <- "C:/stg4-hrap-gis/layers/texas_mrb/area_setup/prepped/cells.gpkg"  # <-- fill in
+key_3   <- "CONUS_subset/config/aoi/texas_mrb/assets/cells.gpkg"  # <-- fill in
+
+raw_3 <- readBin(local_3, what = "raw", n = file.info(local_3)$size)
+s3$put_object(
+  Bucket = bucket,
+  Key    = key_3,
+  Body   = raw_3,
+  ContentType = "application/geopackage+sqlite3"
+  # If that content-type ever gives you grief, swap to:
+  # ContentType = "application/octet-stream"
+)
+message("Uploaded: s3://", bucket, "/", key_3)
